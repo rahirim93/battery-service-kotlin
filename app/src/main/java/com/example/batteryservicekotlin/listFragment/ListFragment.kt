8093276db086e6e5781ea0d37e18604c10be7016
rahirim93 <59837269+rahirim93@users.noreply.github.com>
2022-02-12 23:07:08 +0300

    промежуточный коммит. после создания стабильной версии удалить

--- app/src/main/java/com/example/batteryservicekotlin/listFragment/ListFragment.kt
+++ app/src/main/java/com/example/batteryservicekotlin/listFragment/ListFragment.kt
@@ -14,13 +14,6 @@ import com.example.batteryservicekotlin.database.Unit
 
 class ListFragment : Fragment() {
 
-    private lateinit var listRecyclerView: RecyclerView
-    private var adapter: BatteryAdapter? = null
-
-    private val listViewModel: ListViewModel by lazy {
-        ViewModelProviders.of(this).get(ListViewModel::class.java)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -31,45 +24,7 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_list, container, false)
-
-        listRecyclerView = view.findViewById(R.id.recycler_view) as RecyclerView
-        listRecyclerView.layoutManager = LinearLayoutManager(context)
-
-        updateUI()
-
         return view
     }
 
-    private fun updateUI() {
-
-    }
-
-    private inner class BatteryHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-        val dateTextView: TextView = itemView.findViewById(R.id.textViewItemDate)
-
-    }
-
-    private inner class BatteryAdapter(var units: List<Unit>) : RecyclerView.Adapter<BatteryHolder>() {
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BatteryHolder {
-            val view = layoutInflater.inflate(R.layout.list_item, parent, false)
-            return BatteryHolder(view)
-        }
-
-        override fun getItemCount() = units.size
-
-        override fun onBindViewHolder(holder: BatteryHolder, position: Int) {
-            val unit = units[position]
-            holder.apply {
-                dateTextView.text = unit.date.toString()
-            }
-        }
-    }
-
-    companion object {
-        fun newInstance(): ListFragment {
-            return ListFragment()
-        }
-    }
 }

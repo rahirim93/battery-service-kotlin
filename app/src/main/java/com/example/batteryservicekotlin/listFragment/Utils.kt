7325447cd27ca3245ba33b07617fc45689cce38c
rahirim93 <59837269+rahirim93@users.noreply.github.com>
2022-02-15 01:02:59 +0300

    рабочая версия. добавлена функция получения данных по батарее

--- app/src/main/java/com/example/batteryservicekotlin/listFragment/Utils.kt
+++ app/src/main/java/com/example/batteryservicekotlin/listFragment/Utils.kt
@@ -1,5 +1,11 @@
 package com.example.batteryservicekotlin.listFragment
 
+import android.content.Context
+import android.content.Context.BATTERY_SERVICE
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build
 import com.example.batteryservicekotlin.BatteryBundleForAdapter
 import com.example.batteryservicekotlin.database.Unit
 import java.util.*
@@ -92,4 +98,72 @@ fun getMustNumberInsertsToday(): Int {
     val numberInsertsMustBe = ((calendarInMillis - startDayInMillis) / 1000) / 5
 
     return numberInsertsMustBe.toInt()
+}
+
+/** Функция для получения данных о батарее */
+private fun getBatteryInfo(context: Context) {
+    // Для получения энергии добавить разрешение
+    //<uses- Manifest.permission android:name="android.permission.BATTERY_STATS"
+    //tools:ignore="ProtectedPermissions" />
+
+
+    val batteryManager = context.getSystemService(BATTERY_SERVICE) as BatteryManager
+
+    //Remaining battery capacity as an integer percentage of total capacity (with no fractional part).
+    //Оставшаяся емкость аккумулятора в виде целого процента от общей емкости (без дробной части).
+    val capacityInPercentage = batteryManager
+        .getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+    //log("Процент заряда: $capacityInPercentage")
+
+    // Battery capacity in microampere-hours, as an integer.
+    // Емкость аккумулятора в микроампер-часах, как целое число.
+    val capacityInMicroampereHours = batteryManager
+        .getIntProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER)
+    //log("Емкость в микроампер-часах: $capacityInMicroampereHours")
+
+    // Average battery current in microamperes, as an integer.
+    // Средний ток батареи в микроамперах, как целое число.
+    val currentAverage = batteryManager
+        .getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_AVERAGE)
+    //log("Средний ток потребления: $currentAverage")
+
+    // Instantaneous battery current in microamperes, as an integer.
+    // Мгновенный ток батареи в микроамперах, как целое число.
+    val currentNow = batteryManager
+        .getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
+    //log("Текущий ток потребления: $currentNow")
+
+    // Battery remaining energy in nanowatt-hours, as a long integer.
+    // Оставшаяся энергия батареи в нановатт-часах, как длинное целое число.
+    val energyCounter = batteryManager
+        .getLongProperty(BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER)
+    //log("Оставшаяся энергия: $energyCounter")
+    // Не работает, возможно из-за:
+    // Return the value of a battery property of long type
+    // If the platform does not provide the property queried,
+    // this value will be Long.MIN_VALUE
+
+    // Compute an approximation for how much time (in milliseconds) remains until the battery is fully charged.
+    // Вычмсляет приблизительное время (в миллисекундах), оставшееся до полной зарядки аккумулятора.
+    val chargeTimeRemaining = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        batteryManager.computeChargeTimeRemaining() / 1000
+    } else {
+        TODO("VERSION.SDK_INT < P")
+    }
+    //log("Время зарядки: $chargeTimeRemaining")
+
+
+    val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+    val batteryStatus = context.registerReceiver(null, intentFilter)
+
+    // Extra for Intent.ACTION_BATTERY_CHANGED: integer containing the current battery temperature.
+    // Дополнительно за намерение.ACTION_BATTERY_CHANGED: целое число, содержащее текущую температуру батареи.
+    val temperature = batteryStatus?.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1)
+    //log("Температура: $temperature")
+
+    // Extra for Intent.ACTION_BATTERY_CHANGED: integer containing the current battery voltage level.
+    // Дополнительно за намерение.ACTION_BATTERY_CHANGED: целое число, содержащее текущий уровень напряжения батареи.
+    val voltage = batteryStatus?.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1)
+    //log("Температура: $voltage")
+
 }
